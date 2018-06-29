d799654b69c2779f2c7a14679090d895d1e230a7
degluttiat <alena.liahov@gmail.com>
2018-06-29 21:26:03 +0300

    Refactoring

--- app/src/main/java/com/example/alena/wholeheartedly/QuestionFragment.java
+++ app/src/main/java/com/example/alena/wholeheartedly/QuestionFragment.java
@@ -16,7 +16,6 @@ public class QuestionFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
     private EditText mEditText;
-    private int section;
     private RadioGroup mRadioGroup;
 
     public QuestionFragment() {
@@ -42,11 +41,11 @@ public class QuestionFragment extends Fragment {
 
         switch (x) {
             case 0:
-                return  "2 - 3 quatrains";
+                return getString(R.string.price1);
             case 1:
-                return  "4 - 6 quatrains";
+                return getString(R.string.price2);
             case 2:
-                return  "7 - 10 quatrains";
+                return getString(R.string.price3);
             default:
                 return "";
         }
@@ -54,125 +53,51 @@ public class QuestionFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setRetainInstance(true);
-        View rootView = inflater.inflate(R.layout.fragment_quiz, container, false);
+        int section = getArguments().getInt(ARG_SECTION_NUMBER);
+        View rootView;
 
-        TextView textView = rootView.findViewById(R.id.fragmentTextView);
-        mEditText = rootView.findViewById(R.id.fragmentEditText);
-        section = getArguments().getInt(ARG_SECTION_NUMBER);
-        //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-
-        switch (section) {
-            case 1:
-                textView.setText("Name:");
-
-                break;
-            case 2:
-                textView.setText("Profession:");
-
-                break;
-            case 3:
-                textView.setText("Skills:");
-
-                break;
-            case 4:
-                textView.setText("Hobby:");
-
-                break;
-            case 5:
-                textView.setText("Languages s/he knows:");
-
-                break;
-            case 6:
-                textView.setText("A sphere s/he is an expert in:");
-
-                break;
-            case 7:
-                textView.setText("Favorite book:");
-
-                break;
-            case 8:
-                textView.setText("Favorite movie:");
-
-                break;
-            case 9:
-                textView.setText("Favorite sport activity:");
-
-                break;
-            case 10:
-                textView.setText("Favorite meal:");
-
-                break;
-            case 11:
-                textView.setText("Personality traits that you appreciate:\n\nKindness, beauty, intuition, ability to listen, self criticism etc");
-                mEditText.setHint("");
-
-                break;
-            case 12:
-                textView.setText("Events and recollections:\n\nCircumstances you got to know each other under, a trip you've been to together, help in your moment of need, a surprise made for you etc");
-                mEditText.setHint("");
-
-                break;
-            case 13:
-                textView.setText("Geographic location:\n\nFavorite cafe, bar, pub, etc / Countries, where the person has traveled / City on birth or where s/he living now");
-                mEditText.setHint("");
-
-                break;
-            case 14:
-                textView.setText("Outlook:\n\nIs that person shorter or taller that most other people? / Haircut / Dressing style / Beard / Mustache");
-                mEditText.setHint("");
-
-                break;
-            case 15:
-                textView.setText("Good or bad habits:\n\nLoves to help adult people / Loves to donate / Perfectionism at work / Smoking / Etc!");
-                mEditText.setHint("");
-
-                break;
-            case 16:
-                textView.setText("How has that person impacted you?");
-
-                break;
-            case 17:
-                textView.setText("Similarities or differences between you and that person:\n\nOr, maybe, you would like to emphasize how you complement each other?");
-                mEditText.setHint("");
-
-                break;
-            case 18:
-                textView.setText("What would you like to wish to your dear person?\n\nThese wishes should be related to the event, be it health & wealth wishes for a b-day,healthy relationship, harmony and many children for a wedding day or a successful career for someone who just finished studying in a college etc");
-                mEditText.setHint("");
-
-                break;
-
-            case 19:
-                rootView = inflater.inflate(R.layout.fragment_quiz2, container, false);
-                mRadioGroup = rootView.findViewById(R.id.radioGroup);
-                RadioButton radioButton_1 = rootView.findViewById(R.id.radio_1);
-                RadioButton radioButton_2 = rootView.findViewById(R.id.radio_2);
-                RadioButton radioButton_3 = rootView.findViewById(R.id.radio_3);
-
-                SharedPreferences shPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-                int priceTag = shPref.getInt(getString(R.string.key_price_tag), 0);
-                switch (priceTag) {
-                    case 0:
-                    case 1:
-                        radioButton_1.setChecked(true);
-                        break;
-                    case 2:
-                        radioButton_2.setChecked(true);
-                        break;
-                    case 3:
-                        radioButton_3.setChecked(true);
-                        break;
-                }
-
-                shPref.edit()
-                        .remove(getString(R.string.key_price_tag))
-                        .apply();
-
+        if (section == 19) {
+            rootView = inflater.inflate(R.layout.fragment_quiz2, container, false);
+            setPrices(rootView);
+        } else {
+            rootView = inflater.inflate(R.layout.fragment_quiz, container, false);
+            setQuestion(section, rootView);
         }
 
         return rootView;
+    }
+
+    private void setQuestion(int section, View rootView) {
+        TextView textView = rootView.findViewById(R.id.fragmentTextView);
+        String[] questionsArray = getResources().getStringArray(R.array.questions_array);
+        textView.setText(questionsArray[section]);
+
+        mEditText = rootView.findViewById(R.id.fragmentEditText);
+    }
+
+    private void setPrices(View rootView) {
+        mRadioGroup = rootView.findViewById(R.id.radioGroup);
+        RadioButton radioButton_1 = rootView.findViewById(R.id.radio_1);
+        RadioButton radioButton_2 = rootView.findViewById(R.id.radio_2);
+        RadioButton radioButton_3 = rootView.findViewById(R.id.radio_3);
+
+        SharedPreferences shPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        int priceTag = shPref.getInt(getString(R.string.key_price_tag), 0);
+        switch (priceTag) {
+            case 0:
+            case 1:
+                radioButton_1.setChecked(true);
+                break;
+            case 2:
+                radioButton_2.setChecked(true);
+                break;
+            case 3:
+                radioButton_3.setChecked(true);
+                break;
+        }
+
+        shPref.edit().remove(getString(R.string.key_price_tag)).apply();
     }
 }
--- app/src/main/java/com/example/alena/wholeheartedly/QuizActivity.java
+++ app/src/main/java/com/example/alena/wholeheartedly/QuizActivity.java
@@ -31,45 +31,6 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         setViewPager();
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.quizButtonPrev:
-                mViewPager.arrowScroll(View.FOCUS_LEFT);
-                break;
-            case R.id.quizButtonNext:
-                //-----------------------
-                int currentItem = mViewPager.getCurrentItem();
-                QuestionFragment fragment = (QuestionFragment) mSectionsPagerAdapter
-                        .instantiateItem(mViewPager, currentItem);
-                String text = fragment.getEditTextString();
-                saveTextToShPref(currentItem, text);
-                //----------------------
-                mViewPager.arrowScroll(View.FOCUS_RIGHT);
-                break;
-            case R.id.quizButtonSend:
-                onBtnSendClicked();
-                break;
-        }
-    }
-
-    //------------------
-    private void saveTextToShPref(int currentItem, String text) {
-        SharedPreferences.Editor editor = mShPref.edit();
-        editor.putString("q" + currentItem, text);
-        editor.apply();
-    }
-    //--------------------
-
-    private void onBtnSendClicked() {
-        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                getString(R.string.mailto), getString(R.string.email), null));
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject));
-        emailIntent.putExtra(Intent.EXTRA_TEXT, getAnswersText());
-        startActivity(Intent.createChooser(emailIntent, getString(R.string.chooser_intent_title)));
-
-    }
-
     private void setToolBar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -84,6 +45,49 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         buttonPrev.setOnClickListener(this);
         buttonNext.setOnClickListener(this);
         buttonSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.quizButtonPrev:
+                mViewPager.arrowScroll(View.FOCUS_LEFT);
+                break;
+            case R.id.quizButtonNext:
+                onBtnNextClicked();
+                break;
+            case R.id.quizButtonSend:
+                onBtnSendClicked();
+                break;
+        }
+    }
+
+    private void onBtnNextClicked() {
+        QuestionFragment fragment = getCurrentFragment();
+        String text = fragment.getEditTextString();
+        saveTextToShPref(mViewPager.getCurrentItem(), text);
+        mViewPager.arrowScroll(View.FOCUS_RIGHT);
+    }
+
+    private void saveTextToShPref(int currentItem, String text) {
+        SharedPreferences.Editor editor = mShPref.edit();
+        editor.putString("q" + currentItem, text);
+        editor.apply();
+    }
+
+    private QuestionFragment getCurrentFragment() {
+        int currentItem = mViewPager.getCurrentItem();
+        return (QuestionFragment) mSectionsPagerAdapter
+                .instantiateItem(mViewPager, currentItem);
+    }
+
+    private void onBtnSendClicked() {
+        // TODO Save text when user change fragment by swipe
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                getString(R.string.mailto), getString(R.string.email), null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject));
+        emailIntent.putExtra(Intent.EXTRA_TEXT, getAnswersText());
+        startActivity(Intent.createChooser(emailIntent, getString(R.string.chooser_intent_title)));
     }
 
     private void setViewPager() {
@@ -101,18 +105,21 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onPageSelected(int position) {
-                if (position == 0) {
-                    buttonPrev.setVisibility(View.INVISIBLE);
-                } else {
-                    buttonPrev.setVisibility(View.VISIBLE);
-                }
-
-                if (position == 18) {
-                    buttonNext.setVisibility(View.INVISIBLE);
-                    buttonSend.setVisibility(View.VISIBLE);
-                } else {
-                    buttonNext.setVisibility(View.VISIBLE);
-                    buttonSend.setVisibility(View.INVISIBLE);
+                switch (position) {
+                    case 0:
+                        buttonPrev.setVisibility(View.INVISIBLE);
+                        break;
+                    case 1:
+                        buttonPrev.setVisibility(View.VISIBLE);
+                        break;
+                    case 17:
+                        buttonNext.setVisibility(View.VISIBLE);
+                        buttonSend.setVisibility(View.INVISIBLE);
+                        break;
+                    case 18:
+                        buttonNext.setVisibility(View.INVISIBLE);
+                        buttonSend.setVisibility(View.VISIBLE);
+                        break;
                 }
             }
 
@@ -125,18 +132,14 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private String getAnswersText() {
-        //-----------------------------
         String[] answers = new String[18];
         for (int i = 0; i < answers.length; i++) {
             answers[i] = mShPref.getString("q" + i, "Null");
         }
-        //----------------------------
-        int currentItem = mViewPager.getCurrentItem();
-        QuestionFragment fragment = (QuestionFragment) mSectionsPagerAdapter
-                .instantiateItem(mViewPager, currentItem);
+        QuestionFragment fragment = getCurrentFragment();
 
 
-        String BodyEmail = "Option: " + fragment.getSelectedOptionText() + "\nName: \n" + answers[0] + "\nProfession: \n" + answers[1]
+        return "Option: " + fragment.getSelectedOptionText() + "\nName: \n" + answers[0] + "\nProfession: \n" + answers[1]
                 + "\nSkills: \n" + answers[2] +
                 "\nHobby: \n" + answers[3] + "\nLanguages s/he knows: \n" + answers[4] +
                 "\nA sphere s/he is an expert in: \n" + answers[5] + "\nFavorite book: \n" + answers[6] +
@@ -147,7 +150,6 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
                 "\nHow has that person impacted you? \n" + answers[15] +
                 "\nSimilarities or differences between you and that person: \n" + answers[16] +
                 "\nWhat would you like to wish to your dear person? \n" + answers[17];
-        return BodyEmail;
     }
 
 
