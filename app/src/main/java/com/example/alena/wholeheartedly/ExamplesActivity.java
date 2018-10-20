3e1b1ad12e3ee0ab6e33e1c08ab462a947b7f83b
degluttiat <alena.liahov@gmail.com>
2018-10-20 19:10:48 +0300

    Sync

--- app/src/main/java/com/example/alena/wholeheartedly/ExamplesActivity.java
+++ app/src/main/java/com/example/alena/wholeheartedly/ExamplesActivity.java
@@ -1,18 +1,12 @@
 package com.example.alena.wholeheartedly;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class ExamplesActivity extends AppCompatActivity {
 
@@ -39,44 +33,6 @@ public class ExamplesActivity extends AppCompatActivity {
     }
 
 
-    public static class PlaceholderFragment extends Fragment {
-
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_examples, container, false);
-            TextView textView = rootView.findViewById(R.id.section_label);
-            int section = getArguments().getInt(ARG_SECTION_NUMBER);
-            ConstraintLayout layout = rootView.findViewById(R.id.constraintLayout);
-            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            switch (section) {
-                case 1:
-                    textView.setText(getString(R.string.text2));
-                    break;
-                case 2:
-                    textView.setText(getString(R.string.text1));
-                    break;
-                case 3:
-                    textView.setText(getString(R.string.text));
-                    break;
-            }
-            return rootView;
-        }
-    }
 
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
@@ -88,7 +44,7 @@ public class ExamplesActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int position) {
 
-            return PlaceholderFragment.newInstance(position + 1);
+            return ExamplesFragment.newInstance(position + 1);
         }
 
         @Override
