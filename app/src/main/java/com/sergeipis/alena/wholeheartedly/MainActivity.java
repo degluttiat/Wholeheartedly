ce8a1160fd8381a44a2d609f7f943a79d2948c49
degluttiat <alena.liahov@gmail.com>
2019-01-05 19:20:42 +0200

    Add prices to prices activity

--- app/src/main/java/com/sergeipis/alena/wholeheartedly/MainActivity.java
+++ app/src/main/java/com/sergeipis/alena/wholeheartedly/MainActivity.java
@@ -1,14 +1,36 @@
 package com.sergeipis.alena.wholeheartedly;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener, PurchasesUpdatedListener {
+
+    public static final String SHPREF_PRICES = "Prices";
+    public static final String QUATRAINS_1 = "2_3_quatrains";
+    public static final String QUATRAINS_2 = "4_6_quatrains";
+    public static final String QUATRAINS_3 = "7_10_quatrains";
+
+    private BillingClient mBillingClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +43,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setSupportActionBar(toolbar);
 
         setClickListeners();
+
+        setBilling();
+    }
+
+    private void setBilling() {
+        mBillingClient = BillingClient.newBuilder(this).setListener(this).build();
+        mBillingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(@BillingClient.BillingResponse int responseCode) {
+                if (responseCode == BillingClient.BillingResponse.OK) {
+                    // The billing client is ready. You can query purchases here.
+                    onBillingConnected();
+                }
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        });
+    }
+
+    private void onBillingConnected() {
+        Log.d("ZAQ", "Biling connected");
+        getPrices();
+    }
+
+    private void getPrices() {
+        List<String> skuList = new ArrayList<>();
+        skuList.add(QUATRAINS_1);
+        skuList.add(QUATRAINS_2);
+        skuList.add(QUATRAINS_3);
+        SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+        params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
+        mBillingClient.querySkuDetailsAsync(params.build(),
+                new SkuDetailsResponseListener() {
+                    @Override
+                    public void onSkuDetailsResponse(int responseCode, List<SkuDetails> skuDetailsList) {
+                        onPricesReceived(skuDetailsList);
+                    }
+                });
+    }
+
+    private void onPricesReceived(List<SkuDetails> skuDetailsList) {
+        SharedPreferences.Editor editor = getSharedPreferences(SHPREF_PRICES, MODE_PRIVATE).edit();
+        for (SkuDetails skuDetails : skuDetailsList) {
+            Log.d("ZAQ", "Name: " + skuDetails.getTitle() + " Price: " + skuDetails.getPrice());
+            editor.putString(skuDetails.getSku(), skuDetails.getPrice());
+        }
+        editor.apply();
+    }
+
+    //PurchasesUpdatedListener
+    @Override
+    public void onPurchasesUpdated(int responseCode, @Nullable List<Purchase> purchases) {
+        Log.d("ZAQ", "onPurchasesUpdated");
     }
 
     private void setClickListeners() {
--- app/src/main/java/com/sergeipis/alena/wholeheartedly/PricesActivity.java
+++ app/src/main/java/com/sergeipis/alena/wholeheartedly/PricesActivity.java
@@ -7,9 +7,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 
 public class PricesActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +26,33 @@ public class PricesActivity extends AppCompatActivity implements View.OnClickLis
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
 
+        setViews();
         setClickListeners();
+
+        getPrices();
+    }
+
+    private void setViews() {
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+    }
+
+    private void getPrices() {
+        SharedPreferences pref = getSharedPreferences(MainActivity.SHPREF_PRICES, MODE_PRIVATE);
+        String price1 = pref.getString(MainActivity.QUATRAINS_1, "");
+        String price2 = pref.getString(MainActivity.QUATRAINS_2, "");
+        String price3 = pref.getString(MainActivity.QUATRAINS_3, "");
+
+        button1.setText(String.format("%s - %s", button1.getText(), price1));
+        button2.setText(String.format("%s - %s", button2.getText(), price2));
+        button3.setText(String.format("%s - %s", button3.getText(), price3));
     }
 
     private void setClickListeners() {
-        findViewById(R.id.button1).setOnClickListener(this);
-        findViewById(R.id.button2).setOnClickListener(this);
-        findViewById(R.id.button3).setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
     }
 
     @Override
